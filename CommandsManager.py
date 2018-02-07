e7b819fbc1c97947aefab4437e4037bd085dd9f5
Filipe Ibraim Abdo <filipeibraim@gmail.com>
2018-02-06 23:11:44 -0200

    Ready for USB connection test.

--- CommandsManager.py
+++ CommandsManager.py
@@ -3,7 +3,7 @@ from EffectsManager import *
 class CommandsManager:
     def __init__(self):
         self.CCCommand = [0x2B, 0xB0, 0x00, 0x00]  # 0x2B, 0xB0, CC#n (Change Control Number) , VV (Value)
-        self.PC_Command = [0x2C, 0xC0, 0x00, 0x00]  # 0x2B, 0xB0, CC#n (Change Control Number) , VV (Value)
+        self.PC_Command = [0x2C, 0xC0, 0x00, 0x00]  # 0x2B, 0xB0, PC#n (Patch Control Number) , 0x00
 
     def getCC_Command(self, effect):
         # assert isinstance(effect, EffectsDict)
